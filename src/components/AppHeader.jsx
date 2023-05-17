f0b24af60b74a8d0c4ef4c69c98bf51397c32af9
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-05-17 22:42:37 +0700

    alhamdulillah finish feat: todo filter, animation, bug fixed

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,33 +1,32 @@
-// import { useState } from 'react';
-// import Button, { SelectButton } from './Button';
-// import styles from '../styles/modules/app.module.scss';
-// import TodoModal from './TodoModal';
-// import { useDispatch } from 'react-redux';
-// import { updateFilterStatus } from '../slices/todoSlice';
+import { useState } from 'react';
+import Button, { SelectButton } from './Button';
+import styles from '../styles/modules/app.module.scss';
+import TodoModal from './TodoModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateFilterStatus } from '../slices/todoSlice';
 
-// function AppHeader() {
-//   const [modalOpen, setModalOpen] = useState(false);
-//   const [filterStatus, setFilterStatus] = useState(false);
-//   const dispatch = useDispatch()
+function AppHeader() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const filterStatus = useSelector((state) => state.todo.filterStatus);
+  const dispatch = useDispatch();
 
-//   const updateFilter = () => {
-//     console.log('update select');
-//     dispatch(updateFilterStatus())
-//   };
+  const updateFilter = (e) => {
+    dispatch(updateFilterStatus(e.target.value));
+  };
 
-//   return (
-//     <div className={styles.appHeader}>
-//       <Button variant="primary" onClick={() => setModalOpen(true)}>
-//         Add Task
-//       </Button>
-//       <SelectButton id="status" value={filterStatus} onChange={updateFilter}>
-//         <option value="all">ALL</option>
-//         <option value="incomplete">Incomplete</option>
-//         <option value="complete">Complete</option>
-//       </SelectButton>
-//       <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} type="add" />
-//     </div>
-//   );
-// }
+  return (
+    <div className={styles.appHeader}>
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
+      <SelectButton id="status" value={filterStatus} onChange={updateFilter}>
+        <option value="all">ALL</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Complete</option>
+      </SelectButton>
+      <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} type="add" />
+    </div>
+  );
+}
 
-// export default AppHeader;
+export default AppHeader;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -8,6 +8,15 @@ import { deleteTodo, updateTodo } from '../slices/todoSlice';
 import { toast } from 'react-hot-toast';
 import TodoModal from './TodoModal';
 import CheckButton from './CheckButton';
+import { motion } from 'framer-motion';
+
+const child = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 function TodoItem({ todo }) {
   const dispatch = useDispatch();
@@ -42,7 +51,7 @@ function TodoItem({ todo }) {
 
   return (
     <>
-      <div className={styles.item}>
+      <motion.div className={styles.item} variants={child}>
         <div className={styles.todoDetails}>
           <CheckButton checked={checked} handleCheck={handleCheck} />
           <div className={styles.texts}>
@@ -79,7 +88,7 @@ function TodoItem({ todo }) {
             <MdEdit />
           </div>
         </div>
-      </div>
+      </motion.div>
       <TodoModal
         type="update"
         todo={todo}
